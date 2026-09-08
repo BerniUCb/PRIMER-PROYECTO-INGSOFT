bbcb2afb2ac3c3bb461393dc2a8a969d565a1f78
BerniUCB <bernardo.rios@ucb.edu.bo>
2026-09-08 09:51:58 -0400

    feat: agregar calculo de impuestos por estado

--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -12,4 +12,15 @@ describe("Totalizador de compras", () => {
     // Assert (Afirmar)
     expect(resultado).toEqual(150);
   });
+  it("debería calcular el impuesto para el estado CA (8.25%)", () => {
+    expect(calcularImpuesto(100, "CA")).toEqual(8.25);
+  });
+
+  it("debería calcular el impuesto para el estado TX (6.25%)", () => {
+    expect(calcularImpuesto(100, "TX")).toEqual(6.25);
+  });
+  
+  it("debería calcular el impuesto para el estado AL (4.00%)", () => {
+    expect(calcularImpuesto(100, "AL")).toEqual(4.00);
+  });
 });
