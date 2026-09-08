7ed648a8b521bc50e080b1fc2d4824ecdd2a6966
BerniUCB <bernardo.rios@ucb.edu.bo>
2026-09-08 10:02:23 -0400

    feat: agregar calculo de descuentos segun el monto

--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -1,4 +1,4 @@
-import { calcularPrecioNeto , calcularImpuesto } from "./totalizador.js";
+import { calcularPrecioNeto , calcularImpuesto , calcularDescuento } from "./totalizador.js";
 
 describe("Totalizador de compras", () => {
   it("debería calcular el precio neto multiplicando la cantidad por el precio unitario", () => {
@@ -14,5 +14,24 @@ describe("Totalizador de compras", () => {
   
   it("debería calcular el impuesto para el estado AL (4.00%)", () => {
     expect(calcularImpuesto(100, "AL")).toEqual(4.00);
+  });
+// TESTS PARA EL DESCUENTO
+  it("debería aplicar 0% de descuento para un monto menor a 1000", () => {
+    expect(calcularDescuento(500)).toEqual(0);
+  });
+
+  it("debería aplicar 3% de descuento para un monto de 1000", () => {
+    // El 3% de 1000 es 30
+    expect(calcularDescuento(1000)).toEqual(30); 
+  });
+
+  it("debería aplicar 5% de descuento para un monto de 3000", () => {
+    // El 5% de 3000 es 150
+    expect(calcularDescuento(3000)).toEqual(150); 
+  });
+
+  it("debería aplicar 15% de descuento para un monto de 30000", () => {
+    // El 15% de 30000 es 4500
+    expect(calcularDescuento(30000)).toEqual(4500); 
   });
 });
