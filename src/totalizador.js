33da5779aeaca338767fb7328216cbc3c7d53ca0
BerniUCB <bernardo.rios@ucb.edu.bo>
2026-09-08 09:59:20 -0400

    feat: agregar calculo de impuestos por estado

--- src/totalizador.js
+++ src/totalizador.js
@@ -1,4 +1,16 @@
 export function calcularPrecioNeto(cantidad, precio) {
   return cantidad * precio;
 }
+export function calcularImpuesto(monto, estado) {
+  const tasasDeImpuesto = {
+    UT: 6.65,
+    NV: 8.00,
+    TX: 6.25,
+    AL: 4.00,
+    CA: 8.25
+  };
+
+  const tasa = tasasDeImpuesto[estado] || 0; // Si no existe el estado, el impuesto es 0
+  return monto * (tasa / 100);
+}
 
--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -1,16 +1,8 @@
-import { calcularPrecioNeto } from "./totalizador.js";
+import { calcularPrecioNeto , calcularImpuesto } from "./totalizador.js";
 
 describe("Totalizador de compras", () => {
   it("debería calcular el precio neto multiplicando la cantidad por el precio unitario", () => {
-    // Arrange (Preparar)
-    const cantidad = 3;
-    const precioUnitario = 50;
-
-    // Act (Actuar)
-    const resultado = calcularPrecioNeto(cantidad, precioUnitario);
-
-    // Assert (Afirmar)
-    expect(resultado).toEqual(150);
+    expect(calcularPrecioNeto(3, 50)).toEqual(150);
   });
   it("debería calcular el impuesto para el estado CA (8.25%)", () => {
     expect(calcularImpuesto(100, "CA")).toEqual(8.25);
