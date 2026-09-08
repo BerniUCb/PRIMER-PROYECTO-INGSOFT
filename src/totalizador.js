bbd87a0ebda3aea2eb5ef1dd1bde81fbd5509ed8
BerniUCB <bernardo.rios@ucb.edu.bo>
2026-09-08 10:02:57 -0400

    feat: agregar calculo de descuentos segun el monto

--- src/totalizador.js
+++ src/totalizador.js
@@ -13,4 +13,22 @@ export function calcularImpuesto(monto, estado) {
   const tasa = tasasDeImpuesto[estado] || 0; // Si no existe el estado, el impuesto es 0
   return monto * (tasa / 100);
 }
+export function calcularDescuento(monto) {
+  let porcentaje = 0;
+
+  if (monto >= 30000) {
+    porcentaje = 15;
+  } else if (monto >= 10000) {
+    porcentaje = 10;
+  } else if (monto >= 7000) {
+    porcentaje = 7;
+  } else if (monto >= 3000) {
+    porcentaje = 5;
+  } else if (monto >= 1000) {
+    porcentaje = 3;
+  }
+
+  // Devolvemos de cuánto es el descuento en dinero, no solo el porcentaje
+  return monto * (porcentaje / 100);
+}
 
